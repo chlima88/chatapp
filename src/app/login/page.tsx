67b29f4a5d978285e6513e13481974db41989b50
Charles Costa <chlima88@gmail.com>
2023-06-07 03:16:11 -0300

    updates de estilos da tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/db";
+import { Icon } from "@iconify/react";
 
 export default function Page() {
   const [signInWithGoole, user, loading] = useSignInWithGoogle(auth);
@@ -15,15 +16,16 @@ export default function Page() {
     redirect("/cadastro")
   ) : (
     <div className="flex w-full items-center justify-center h-screen">
-      <div className="flex flex-col bg-violet-500 p-2">
-        <div className="w-full text-center text-2xl text-gray-50 p-2">
-          Welcome to <span className="font-semibold">ChatApp!</span>
+      <div className="flex flex-col gap-4 border-violet-500  p-4 rounded-md">
+        <div className="w-full text-center text-2xl text-gray-600 p-4">
+          Welcome to <span className="font-semibold text-3xl">ChatApp!</span>
         </div>
         <button
-          className="bg-white p-2 rounded-sm font-medium"
+          className="flex items-center justify-center gap-4 bg-white text-violet-500  border-violet-500 hover:bg-violet-500 hover:text-white transition-colors  border-[1px] p-2 rounded-sm font-medium"
           onClick={handleSignIn}
         >
-          Sign-In with Google
+          <Icon icon="devicon:google" />
+          <div className="text-center">Sign-In with Google</div>
         </button>
       </div>
     </div>
